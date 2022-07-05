a1ef073ce17503d7b4b5a412fb9909ed09d75490
Philip Massouh <philipmassouh@gmail.com>
2022-07-05 15:35:05 -0700

    deleted an implementation that wasnt terrible enough

--- parity.py
+++
@@ -1,5 +0,0 @@
-n = input("Please enter an integer: ")
-while (not n.isdigit()) and not (n[0] == '-' and (n := n[1:]).isdigit()):
-    n = input(f"You entered {n}, that is not an integer you imbecile! Try again: ")
-print(f"{n} is {('even', 'odd')[(int(n) % 2)]}!")
-
